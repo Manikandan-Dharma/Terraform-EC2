de7903b2b2f65538b64f9402e44e7a21c6515f84
Manikandan-Dharma <157537135+Manikandan-Dharma@users.noreply.github.com>
2024-02-19 12:24:00 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = "us-east-1" 
 }
 
-resource "aws_instance" "CICD-SERVER" {
+resource "aws_instance" "CICD-SERVER2" {
   ami           = "ami-0e731c8a588258d0d"   
   instance_type = "t2.micro" 
   key_name = "FINAL KEYPAIR"
   monitoring = true
 
   tags = {
-    Name = "CICD-SERVER"
+    Name = "CICD-SERVER2"
   }
 }
